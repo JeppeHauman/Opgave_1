2b3eca3618e19ec6961e16cea70acae8717b2701
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-03-02 09:29:01 +0100

    Update BeerTests.cs
    More tests to Beer class

--- Opgave_1Tests/BeerTests.cs
+++ Opgave_1Tests/BeerTests.cs
@@ -23,6 +23,12 @@ namespace Opgave_1.Tests
             Assert.AreEqual(123, beer.Id);
             Assert.AreNotEqual("Tubor", beer.Name);
             Assert.ThrowsException<ArgumentException>(() => beer.Name = "tub");
+            Assert.AreEqual(20, beer.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Price = -2);
+            Assert.AreEqual(5.2, beer.Abv);
+            Assert.AreNotEqual(5.1999, beer.Abv);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Abv = 101);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Abv = -1);
         }
     }
 }
