e44c609c896ca916095e94f11bebf58044584697
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-03-02 09:23:06 +0100

    Update BeerTests.cs
    Made the first couple tests to Beer class

--- Opgave_1Tests/BeerTests.cs
+++ Opgave_1Tests/BeerTests.cs
@@ -9,10 +9,20 @@ namespace Opgave_1.Tests
     [TestClass()]
     public class BeerTests
     {
-        [TestMethod()]
+        private Beer beer;
+
+        [TestInitialize]
+        public void init()
+        {
+            beer = new Beer(123, "Tuborg", 20, 5.2);
+        }
+
+            [TestMethod()]
         public void BeerTest()
         {
-            Assert.Fail();
+            Assert.AreEqual(123, beer.Id);
+            Assert.AreNotEqual("Tubor", beer.Name);
+            Assert.ThrowsException<ArgumentException>(() => beer.Name = "tub");
         }
     }
 }
