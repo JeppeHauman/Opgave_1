80c8ffd5ad6f4fb728083ab6a16f242c99779601
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-03-02 09:12:52 +0100

    Oprettet tests

---
+++ Opgave_1Tests/BeerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave_1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave_1.Tests
+{
+    [TestClass()]
+    public class BeerTests
+    {
+        [TestMethod()]
+        public void BeerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
